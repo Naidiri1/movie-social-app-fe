078cdedbc05e9a83ab60c933686691086386bf1c
Iris <iridian95@live.com>
2025-07-13 22:22:20 -0400

    adding popular and query fetching mnp

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,49 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { Button, Card, Input, Typography } from "@material-tailwind/react";
+
 
 export default function Home()  {
+    const [movieQuery, setMovieQuery] = useState<string>('');
 
-return (
-    <div>
-        main page
-    </div>
-)
+    const searchMovies = async () => {
+    const response = await fetch(`http://localhost:8080/api/movies/search?query=${movieQuery}`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+    } 
+
+    if(response.ok){
+        const data = await response.json();
+        console.log(data)
+    }
+
+    }
+
+    const handleSearchPopularMovies = async () => {
+      const response = await fetch(`http://localhost:8080/api/movies/popular`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch movies');
+    } 
+
+    if(response.ok){
+        const data = await response.json();
+        console.log(data)
+    }
+
+    }  
+    
+    return (
+        <div>
+        <Input
+        label='Search'
+        value={movieQuery}
+        onChange={(e: any)=> setMovieQuery(e.target.value)}
+        />
+        <Button onClick={searchMovies}>Search</Button>
+        <Button onClick={handleSearchPopularMovies}>Popular</Button>
+        </div>
+    )
 }
