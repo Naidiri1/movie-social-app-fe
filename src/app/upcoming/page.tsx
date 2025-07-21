304b8704e6f22459f596cccdbd5aa20596865804
Iris <iridian95@live.com>
2025-07-20 23:44:20 -0400

    added upcoming api and movie card display

--- src/app/upcoming/page.tsx
+++ src/app/upcoming/page.tsx
@@ -1,21 +1,44 @@
 'use client'
 
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import  CardMovie  from '../../components/CardMovie';
 
 const UpcomingMovies = () => {
+    const [rowData, setRowData] = useState([]);
+
+    const handleSearchPopularMovies = async () => {
+      const response = await fetch(`http://localhost:8080/api/movies/upcoming`);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch movies');
+        } 
+        if(response.ok){
+            const data = await response.json();
+            const results = data.results;
+            console.log(results)
+            setRowData(results)
+        }
+    };
 
    useEffect(() => {
-
+    handleSearchPopularMovies();
    },[]);
 
-   
-
-
     return(
-    <div>
-        
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-none">
+    {rowData.map((movie:any) => (
+            <CardMovie
+            key={movie.id}
+            movieId={movie.id}
+            movieTitle={movie.title}
+            movieDescription={movie.overview}
+            movieScore={movie.vote_average}
+            imgPoster={movie.poster_path}
+            movieRelease={movie.release_date}
+            />
+        ))}
     </div>
     )
-}
+    }
 
 export default UpcomingMovies
