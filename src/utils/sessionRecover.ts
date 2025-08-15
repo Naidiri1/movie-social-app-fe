07a6ad232c8e0f719184ba4025409fd786148e00
Iris <iridian95@live.com>
2025-08-15 12:37:45 -0400

    debugged session

--- src/utils/sessionRecover.ts
+++ src/utils/sessionRecover.ts
@@ -1,15 +1,19 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { restoreUserSession , selectUsername } from "../redux/reducers/authSlice";
-
+import { restoreUserSession, selectUsername } from "../redux/reducers/authSlice";
 
 export const useFetchUserIfNull = () => {
-    const user = useSelector(selectUsername);
+    const username = useSelector(selectUsername);
     const dispatch: any = useDispatch();
 
     useEffect(() => {
-        if (user === null) {
-            dispatch(restoreUserSession());
+        if (username === null) {
+            const token = sessionStorage.getItem('access_token');
+            console.log('Token in hook:', token); // ADD THIS
+            if (token) {
+                console.log('Dispatching restoreUserSession'); // ADD THIS
+                dispatch(restoreUserSession());
+            }
         }
-    }, [selectUsername]);
+    }, [username, dispatch]);
 };
